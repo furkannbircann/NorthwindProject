bbf7353b704147a27c68ed086b6e74b34c91c71f
Furkan Bircan <furkhanbircan@gmail.com>
2022-05-25 14:28:13 +0300

    FinalProject - Core Layer Added / New Object Defined (with JOIN)

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,8 +9,31 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            //ProductGetAllTest();
+            //CategoryTest();
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var item in productManager.GetByUnitPrice(40,100))
+            foreach (var item in productManager.GetProductDetails())
+            {
+                Console.WriteLine("{0} / {1} - {2} - {3}" ,item.ProductId, item.ProductName ,item.CategoryName,item.UnitsInStock );
+            }
+
+
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var c in categoryManager.GetAll())
+            {
+                Console.WriteLine(c.CategoryName);
+            }
+            Console.WriteLine("{0} / 2.category", categoryManager.GetById(2).CategoryName);
+        }
+
+        private static void ProductGetAllTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var item in productManager.GetAll())
             {
                 Console.WriteLine(item.ProductID + " Id'li " + item.ProductName);
             }
--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Abstract;
+﻿
+using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,13 +7,13 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
     //Generic Constraint
     //class = referans tip
     //IEntity = IEntity olabilir veya implement olanlar olabilir
     //New() = new lenebilir olması gerekir.
-    public interface IEntityRepository<T> where T:class,IEntity,new() 
+    public interface IEntityRepository<T> where T : class, IEntity, new()
     {
         List<T> GetAll(Expression<Func<T,bool>> filter = null);
         T Get(Expression<Func<T, bool>> filter);
